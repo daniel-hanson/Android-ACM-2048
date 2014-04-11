eecb94d2999f57ce892b2f61773fde40eb2a8183
Jordan Backes <jjbxtd@mail.missouri.edu>
2014-04-10 20:01:39 -0500

    Update Grid.java

--- src/com/mizzou/acm/android/Grid.java
+++ src/com/mizzou/acm/android/Grid.java
@@ -1,46 +1,67 @@
 package com.mizzou.acm.android;
 
-import java.util.ArrayList;
-
 
 public class Grid
 {
 	Gravity gravity = null;
-	private static final int SIZE = 4;		//How many rows and columns there are in the grid
 	
-	ArrayList<ArrayList<Cell>> buildGrid ()
+	
+	Cell[][] buildGrid ()
 	{
-		ArrayList<ArrayList<Cell>> list = new ArrayList<ArrayList<Cell>>();
+		ArrayList<ArraryList<Cell>> list = ArrayList<ArrayList<Cell>>();
 		
-		for(int x = 0; x < SIZE; x++)
+		for(int x = 0; x<SIZE; x++)
 		{
-			ArrayList<Cell> temp = new ArrayList<Cell>();
-			for (int y = 0; y < SIZE; y++)
-				temp.add(new Cell());
-			
-			list.add(temp);
-		}		
-		
-		return list;
-	}
-
-	Cell[][] fromState(Cell[][] state)
-	{
-		Cell cells[][] = buildGrid();
-		for(int x = 0; x < SIZE; x++)
-			for (int y = 0; y < SIZE; y++)
+			ArrayList<Cell> temp  = new ArrayList<Cell>();
+			for (y = 0; y<SIZE; y++)
 			{
-				cells[x][y] = state[x][y];
-				//Cell cells[x][y] = (tile ? new Tile(tile.position, tile.value): new Cell());
-				
+				temp.add( new Cell());
+				list.add(temp);
 			}
-<<<<<<< HEAD
-		
+		}
 		return cells;
 	}
-=======
+
+	Cell [] fromState (state)
+	{
+		ArrayList<ArrayList<Cell>> list = ArrayList<ArrayList<Cell>>();
+		{
+			for(int x = 0; x < SIZE; x++)
+			{
+				ArrayList<Cell> temp = new ArrayList<Cell>();
+				for(int y = 0; y < SIZE; y++)
+				{
+					Cell tile[x][y] = state[x][y];
+					temp.add(tile ? new Cell(cell.position,cell.value) : new Cell());
+					list.add(temp);
+				
+				}
+			}
 		}
 		return cells;
 	}	
->>>>>>> f96f2ea70a7120d79e9bc9181d1c4648a8eb6ae6
+
+	Cell[] findRand ()
+	{
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
