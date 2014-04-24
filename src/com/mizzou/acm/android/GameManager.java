1004a68bdbf7ef1f5dc6f220bbfab17421c6599c
Lee Presswood <pressedlee123@gmail.com>
2014-04-24 18:46:44 -0500

    Created GameManager class for game logic

---
+++ src/com/mizzou/acm/android/GameManager.java
@@ -0,0 +1,10 @@
+//This is the class that controls all the game logic
+//From the main Android activity, we will call the update in this logic.
+//After the update, we will update the drawing.
+package com.mizzou.acm.android;
+
+public class GameManager
+{
+	
+	
+}
