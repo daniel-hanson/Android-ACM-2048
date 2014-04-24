d13d937de72b93c997535761d1a678f82d324333
Lee Presswood <pressedlee123@gmail.com>
2014-04-24 18:55:20 -0500

    Created GameLogic class. Last commit is wrong.
    GameManager now deals with the order in which things are initialized,
    rendered, updated, and checked.

---
+++ src/com/mizzou/acm/android/GameLogic.java
@@ -0,0 +1,12 @@
+//This is the class that controls all the game logic
+//From the main Android activity, we will first look for a gesture.
+//The gesture tells the manager we want an update. 
+//We will call the update in this class.
+//After the game update, we will render the drawing for the game's current state.
+
+package com.mizzou.acm.android;
+
+public class GameLogic
+{
+
+}
--- src/com/mizzou/acm/android/GameManager.java
+++ src/com/mizzou/acm/android/GameManager.java
@@ -1,6 +1,12 @@
-//This is the class that controls all the game logic
-//From the main Android activity, we will call the update in this logic.
-//After the update, we will update the drawing.
+/* This is the game manager class. It deals with all the rendering and updating.
+ * The game will not update until a gesture is made. These finite states
+ * make the logic in the game much easier to understand.
+ * When a gesture is made:
+ * The update will be called first. The render is called afterward. 
+ * After the above two, the state will be checked to see if we have won or lost.
+ * Full board = Lose.
+ * 2048 Tile Existing = Win. -->User may continue on or reset.
+ */
 package com.mizzou.acm.android;
 
 public class GameManager
