24b4f8c89af09ab3cc23a59d07b55072dbe6eea3
Lee Presswood <pressedlee123@gmail.com>
2014-04-24 19:34:35 -0500

    Finished win checking in GameChecker

--- src/com/mizzou/acm/android/helpers/GameCheck.java
+++ src/com/mizzou/acm/android/helpers/GameCheck.java
@@ -28,6 +28,14 @@ public class GameCheck
 		//Scan for the 2048 tile
 		for(int i = 0; i < 4; i++)
 			for(int j = 0; j < 4; j++)
-				if(grid.
+				if(grid.getCellValue(i, j) == 2048)
+				{
+					//Set the hasWon boolean for future checks
+					this.hasWon = true;
+					return true;
+				}
+		
+		//If we made it here, we did not win.
+		return false;
 	}
 }
