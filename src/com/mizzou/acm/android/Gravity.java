7c287d5c8146ba8f13395632421a8b85bf4cb483
Lee Presswood <pressedlee123@gmail.com>
2014-04-25 08:31:14 -0500

    Explained gravity in comments

--- src/com/mizzou/acm/android/Gravity.java
+++ src/com/mizzou/acm/android/Gravity.java
@@ -1,5 +1,8 @@
+/* This simple enum determines the gravity of the board.
+ * Eventually, we will want to be changing the gravity during the game logic step.
+ * all the tiles will shift in the direction of the gravity until they can't move anymore.
+ */
 package com.mizzou.acm.android;
-
 
 public enum Gravity
 {
