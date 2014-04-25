01ba6858e9e9d156a91c595832ad6878b6ea0690
Lee Presswood <pressedlee123@gmail.com>
2014-04-25 09:48:11 -0500

    Began working on GameLogic

--- src/com/mizzou/acm/android/helpers/GameLogic.java
+++ src/com/mizzou/acm/android/helpers/GameLogic.java
@@ -3,10 +3,25 @@
 //The gesture tells the manager we want an update. 
 //We will call the update in this class.
 //After the game update, we will render the drawing for the game's current state.
-
 package com.mizzou.acm.android.helpers;
 
 public class GameLogic
 {
-
+  //This class will hold and act upon the grid
+  private Grid grid;
+  
+  public GameLogic()
+  {
+    
+  }
+  
+  public void reset()
+  {
+    
+  }
+  
+  public Grid getGrid()
+  {//Use this getter to get the grid. This will be useful for the drawing and checking phases
+    return this.grid;
+  }
 }
