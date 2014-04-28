00188449f0ed74ce3e27631823a8dcacabe9edb7
Lee Presswood <pressedlee123@gmail.com>
2014-04-28 16:52:30 -0500

    Explained gamerender

--- src/com/mizzou/acm/android/helpers/GameRender.java
+++ src/com/mizzou/acm/android/helpers/GameRender.java
@@ -1,3 +1,6 @@
+/* This class will deal with the drawing of the game after its state has been updated.
+   Call the render() method after you have finished updating.
+*/
 package com.mizzou.acm.android.helpers;
 
 public class GameRender
