72d64f60b25a5a4829427ec80b7f6c163373b40e
Lee Presswood <pressedlee123@gmail.com>
2014-05-01 18:43:51 -0500

    Fixed cell's errors

--- src/com/mizzou/acm/android/Cell.java
+++ src/com/mizzou/acm/android/Cell.java
@@ -13,20 +13,23 @@ public class Cell
 	private int y;
 	
 	private Position previousPosition;
-	private Position position;		//Postion on the screen using grid coordinates. (Example: First cell is [0][0])
+	private Position position;		//Position on the screen using grid coordinates. (Example: First cell is [0][0])
 	
 	private int value = 2;
-	
-	public Cell()
-	{
-		
-	}
+	private int mergedFrom;
 	
 	public Cell(Position position, int value)
 	{
 		this.previousPosition = null;
 		this.updatePosition(position);
 		this.value = value;
+		
+		this.x = position.x;
+		this.y = position.y;
+		this.value = value;
+		
+		this.previousPosition = null;
+		this.mergedFrom = 1; // Tracks tiles that merged together
 	}
 	
 	public void updatePosition(Position position)
@@ -40,18 +43,9 @@ public class Cell
 		//This can be done by getting the X and Y position of the grid, moving forward by (size of the gap) * (position + 1)
 	}
 
-function Tile(position, value) {
-	  this.x                = position.x;
-	  this.y                = position.y;
-	  this.value            = value || 2;
-
-	  this.previousPosition = null;
-	  this.mergedFrom       = null; // Tracks tiles that merged together
-	}
-
 	void savePosition()
 	{
-		this.previousPosition = { x: this.x, y: this.y };
+		this.previousPosition = position;
 	}
 	
 }
