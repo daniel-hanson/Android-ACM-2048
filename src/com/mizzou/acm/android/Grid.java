c650c6aa08564018ee3800e43e46bd12e0383567
Lee Presswood <pressedlee123@gmail.com>
2014-05-01 18:37:57 -0500

    Style changes

--- src/com/mizzou/acm/android/Grid.java
+++ src/com/mizzou/acm/android/Grid.java
@@ -5,7 +5,7 @@ public class Grid
 	private Gravity gravity = null;
 	private static final int SIZE = 4;		//How many rows and columns there are in the grid
 	
-	Cell[][] buildGrid ()
+	Cell[][] buildGrid()
 	{
 		Cell[][] cells = new Cell[4][];
 		
@@ -29,7 +29,6 @@ public class Grid
 			{
 				
 				cells[x][y] = state[x][y];
-				//Cell cells[x][y] = (tile ? new Tile(tile.position, tile.value): new Cell());
 			}
 		}
 		
